d99f1bc3e43636ea22ae07414d76481b892dd318
Michele Marcucci <michele@befree.it>
2018-10-31 12:51:19 +0100

    Responsive widgets

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import {
   Badge,
   Button,
@@ -13,57 +14,82 @@ import {
 import { Trans } from '@lingui/macro';
 
 class Dashboard extends Component {
+  constructor () {
+    super();
+    this.state = {
+      widgetClasses: 'd-inline-block text-muted text-truncate'
+    }
+  }
+
+  updateDimensions() { 
+    const widgets = ReactDOM.findDOMNode(this.refs.main).getElementsByClassName('widget');
+    const smallWidth = (widgets[0] && widgets[0].offsetWidth && widgets[0].offsetWidth <= 280) || false;
+    let widgetClasses = this.state.widgetClasses;
+
+    if (smallWidth) widgetClasses += ' small-width'
+
+    this.setState({ widgetClasses: widgetClasses });
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions.bind(this));
+  }
 
   render() {
 
     return (
-      <div>
+      <div ref="main">
         <div className="animated fadeIn">
           <Row>
             <Col xs="12" md="6" xl="3">
               <Card className="bg-dark">
-                <CardBody>
+                <CardBody className="widget">
                   <div className="h1 text-muted float-right"><i className="fa fa-fire text-gray"></i></div>
                   <div className="h4 m-0">135.87 MH/s</div>
                   <div><Trans>Current hashrate</Trans></div>
                   <Progress className="progress-xs my-3" color="primary" value="95" />
-                  <small className="d-inline-block text-muted text-truncate"><Trans>Pool</Trans>: <b>stratum+tcp://us.litecoinpool.org:3333</b></small>
+                  <small className={this.state.widgetClasses}><Trans>Pool</Trans>: <b>stratum+tcp://us.litecoinpool.org:3333</b></small>
                 </CardBody>
               </Card>
             </Col>
 
             <Col xs="12" md="6" xl="3">
               <Card className="bg-info">
-                <CardBody>
+                <CardBody className="widget">
                   <div className="h1 text-muted float-right"><i className="fa fa-thermometer-half text-gray"></i></div>
                   <div className="h4 m-0">67°C</div>
                   <div><Trans>Miner temperature</Trans></div>
                   <Progress className="progress-xs my-3" color="success" value="67" />
-                  <small className="d-inline-block text-muted text-truncate"><Trans>MCU temperature</Trans>: <b>57°C</b></small>
+                  <small className={this.state.widgetClasses}><Trans>MCU temperature</Trans>: <b>57°C</b></small>
                 </CardBody>
               </Card>
             </Col>
 
             <Col xs="12" md="6" xl="3">
               <Card className="bg-gray-300">
-                <CardBody>
+                <CardBody className="widget">
                   <div className="h1 text-muted float-right"><i className="fa fa-exclamation-triangle text-gray"></i></div>
                   <div className="h4 m-0">20.59%</div>
                   <div><Trans>Rejected shares</Trans></div>
                   <Progress className="progress-xs my-3" color="warning" value="20.6" />
-                  <small className="d-inline-block text-muted text-truncate"><Trans>Errors</Trans>: <b>0.23%</b></small>
+                  <small className={this.state.widgetClasses}><Trans>Errors</Trans>: <b>0.23%</b></small>
                 </CardBody>
               </Card>
             </Col>
 
             <Col xs="12" md="6" xl="3">
               <Card className="bg-gray-200">
-                <CardBody>
+                <CardBody className="widget">
                   <div className="h1 text-muted float-right"><i className="fa fa-clock text-gray"></i></div>
                   <div className="h4 m-0">3 days</div>
                   <div><Trans>Miner uptime</Trans></div>
                   <Progress className="progress-xs my-3" color="success" value="100" />
-                  <small className="d-inline-block text-muted text-truncate"><Trans>Last share</Trans>: <b>3 seconds</b> ago</small>
+                  <small className={this.state.widgetClasses}><Trans>Last share</Trans>: <b>3 seconds</b> ago</small>
                 </CardBody>
               </Card>
             </Col>
