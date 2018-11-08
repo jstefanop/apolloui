1b4d0582cc5bedb2529e1a018da19802a8fe0104
Michele Marcucci <michele@befree.it>
2018-11-08 09:39:08 +0100

    Build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.208779b6e503662497026b9a6ab69d72.js"
+  "/precache-manifest.86224d6d72f8cecc44d73901c45bcbd9.js"
 );
 
 workbox.clientsClaim();
