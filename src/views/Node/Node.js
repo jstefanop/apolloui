69da1e87ab35a82ef2abb786ee6b20e14e131f6a
Alex Thomas <alex@areios.io>
2020-03-07 21:04:38 -0500

    Replace Node placeholder with actual component

--- src/views/Node/Node.js
+++ src/views/Node/Node.js
@@ -5,90 +5,87 @@ import {
   Card,
   CardBody,
   Col,
-  Row
+  Row,
+  Table
 } from 'reactstrap';
 
-import _ from 'lodash';
-import moment from 'moment';
-
-import { LoadingErrorBox } from '../Loading';
-import DashboardWidget from '../Widgets/DashboardWidget';
-import { convertTemp, displayHashrate, minerModeIcon, tempColor } from '../Filters';
-import PoolsTable from '../Pools/PoolsTable';
-
+import { I18n } from "@lingui/react";
 import { Trans } from '@lingui/macro';
 
 class Node extends Component {
-
   render() {
-    const { minerCheck, minerError, mcuError, mcu, miner, settings } = this.props;
-
-    // Miner shares
-    const minerpercentageRejected = miner.stats.summary.data.deviceRejected;
-    const minerpercentageError = miner.stats.summary.data.deviceHardware;
-    let errorsColor = 'success';
-    if (minerpercentageError >= 5 && minerpercentageError <= 7.5) errorsColor = 'warning';
-    else if (minerpercentageError > 7.5) errorsColor = 'danger';
-
-    // Miner uptime
-    const minerUptime = moment().to(moment().subtract(miner.stats.summary.data.elapsed, 'seconds'), true);
-
-    // Active pool
-    const mainPool = _.find(miner.stats.pools.data, function(o) { return o.lastShareTime > 0 && o.stratumActive === true; });
-
-    // Last share
-    let lastShare = 'Not available',
-        lastShareTime = 0,
-        lastShareColor = 'success';
-    const timestamp = moment().format('X');
-    if (mainPool && mainPool.lastShareTime) {
-      lastShare = moment().to(moment(mainPool.lastShareTime, 'X'));
-      lastShareTime = mainPool.lastShareTime;
-    }
-    const diffLastShare = timestamp - lastShareTime;
-    if (diffLastShare >= 300 && diffLastShare <= 600) lastShareColor = 'warning';
-    else if (diffLastShare > 600) lastShareColor = 'danger'
+    // TODO: Use loadingNode
+    const { loadingNode, node, nodeError } = this.props;
 
     return (
-      <div ref="main">
-        { (mcuError) ?
-            <Alert color="warning">There is a problem fetching system stats (<b>{ mcuError }</b>)</Alert>
-          : null
-        }
-        <div className="animated fadeIn">
+      <div ref='main'>
+        {nodeError && <Alert color='warning'>There is a problem fetching system stats (<b>{nodeError}</b>)</Alert>}
+        <div className='animated fadeIn'>
           <Row>
-            <Col xs="12" md="6" xl="3">
-              <DashboardWidget
-                bgColor="bg-dark"
-                icon="fa fa-fire"
-                value={ displayHashrate(miner.stats.summary.data.workUtility * 71582788, 'h') }
-                title="Current hashrate"
-                progressColor="primary"
-                progressValue="100"
-                secondaryTitle="Pool"
-                secondaryValue={(mainPool && mainPool.url) ? mainPool.url : 'Nothing active'}
-              ></DashboardWidget>
+            <Col xs='12' md='6'>
+              <Card className="bg-light">
+                <CardBody>
+                  <div className="h4 m-0">{node.stats.blockCount}</div>
+                  <div><Trans>Blocks</Trans></div>
+                </CardBody>
+              </Card>
+            </Col>
+
+            <Col xs='12' md='6'>
+              <Card className="bg-light">
+                <CardBody>
+                  <div className="h4 m-0">{node.stats.connectionCount}</div>
+                  <div><Trans>Connections</Trans></div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <h4><Trans>Connections</Trans></h4>
+              <div>
+                <I18n>
+                  {({ i18n }) => (
+                    <Table responsive className="table-outline d-table d-sm-table">
+                      <thead className="bg-light">
+                        <tr>
+                          <th><Trans>IP</Trans></th>
+                          <th><Trans>Client</Trans></th>
+                        </tr>
+                      </thead>
+                      <tbody className="bg-white">
+                        {node.stats.peerInfo.map(function(peer, index) {
+                          return (
+                            <tr key={index}>
+                              <td>
+                                <div className="font-weight-bold text-muted">{peer.addr}</div>
+                              </td>
+                              <td>
+                                {peer.subver}
+                              </td>
+                            </tr>
+                          )
+                        })}
+                      </tbody>
+                    </Table>
+                  )}
+                </I18n>
+              </div>
             </Col>
           </Row>
         </div>
-
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    loadingMcu: state.mcuStats.loading,
-    mcu: state.mcuStats.data,
-    mcuError: state.mcuStats.error,
-    loadingMiner: state.minerStats.loading,
-    miner: state.minerStats.data,
-    minerError: state.minerStats.error,
-    loadingOnline: state.minerOnline.loading,
-    minerCheck: state.minerOnline.data,
-    settings: state.settings
+    loadingNode: state.nodeStats.loading,
+    node: state.nodeStats.data,
+    nodeError: state.nodeStats.error
   }
-}
+};
 
 export default connect(mapStateToProps)(Node);
