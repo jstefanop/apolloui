8b64384a8c726cce3a764a44e2d759fa292833d6
Alex Thomas <alex@areios.io>
2020-03-20 17:12:45 -0400

    Remove Connections CardBody

--- src/views/Node/Node.js
+++ src/views/Node/Node.js
@@ -126,17 +126,6 @@ class Node extends Component {
           </Row>
 
           <Row>
-            <Col xs='12' md='6'>
-              <Card className="bg-light">
-                <CardBody>
-                  <div className="h4 m-0">{connectionCount}</div>
-                  <div><Trans>Connections</Trans></div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-
-          <Row>
             <Col>
               <h4><Trans>Connections</Trans></h4>
               <div>
