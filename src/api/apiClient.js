bd6d872d4fcb7dc9df3aad155c5200870671c478
jstefanop <john@future-apps.net>
2021-12-09 12:31:58 -0500

    Update Strings

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -4,7 +4,7 @@ import { handleApiResponse } from './shared';
 
 const { hostname } = new URL(window.location.href);
 
-const graphqlEndpoint = `http://${hostname}:${process.env.REACT_API_PORT || 5001}/api/graphql`;
+const graphqlEndpoint = `http://${hostname}:${process.env.PORT || 5000}/api/graphql`;
 
 export async function query({
   query: _query, variables, path, accessToken,
